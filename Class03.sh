56d9200c74d6669a1c6ed7ff2ba58a1020636fbb
Mejtus1 <GitMaster1769@proton.me>
2023-10-31 22:58:53 +0100

    class02 and class03

---
+++ Class03.sh
@@ -0,0 +1,8 @@
+# Variables 
+myname="Jay" # variable named myname with value string "Jay"
+echo "Hello World!" # Hello World!
+# - we can use echo to print variables 
+# - whenever we refference variable in bash we need to put it inside $ 
+echo myname # myname
+echo $myname # Jay
+echo $newvariable # empty because we havent declared it, it doesnt exist 
